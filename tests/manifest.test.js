c0fefd62e8fbb174d5b88071b4d2a88e0a3bf7b0
Alessandro Surace <zioalex@gmail.com>
2025-07-31 01:17:23 +0200

    Bump version to 1.1.4

--- tests/manifest.test.js
+++ tests/manifest.test.js
@@ -19,7 +19,7 @@ describe('Manifest.json', () => {
   test('should have required fields', () => {
     expect(manifest.manifest_version).toBe(2);
     expect(manifest.name).toBe('Trello Task Creator');
-    expect(manifest.version).toBe('1.1.3');
+    expect(manifest.version).toBe('1.1.4');
     expect(manifest.description).toBeDefined();
   });
 
